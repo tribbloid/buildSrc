d2b415d78689787865e0355115596b2cebabbb6a
tribbloid <peng.cheng@acyclic.ai>
2025-01-07 15:55:09 -0500

    upgrade com.github.ben-manes.versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
 
-    implementation("com.github.ben-manes.versions:com.github.ben-manes.versions.gradle.plugin:0.49.0")
+    implementation("com.github.ben-manes.versions:com.github.ben-manes.versions.gradle.plugin:0.51.0")
     implementation("io.github.gradle-nexus.publish-plugin:io.github.gradle-nexus.publish-plugin.gradle.plugin:1.3.0")
     implementation("io.github.cosmicsilence.scalafix:io.github.cosmicsilence.scalafix.gradle.plugin:0.1.14")
 }

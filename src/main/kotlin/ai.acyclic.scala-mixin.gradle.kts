0bb5f4d7356b75bccd7198c38855e3b31b31b0bb
Peng Cheng <peng.cheng@acyclic.ai>
2025-02-17 17:19:39 -0500

    upgrade scalameta

--- src/main/kotlin/ai.acyclic.scala-mixin.gradle.kts
+++ src/main/kotlin/ai.acyclic.scala-mixin.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     testImplementation("org.scalameta:scalafmt-interfaces:3.8.6")// only used for prompting upgrade
 }
 
-val scalametaV = "4.12.7"
+val scalametaV = "4.13.1.1"
 
 allprojects {
 

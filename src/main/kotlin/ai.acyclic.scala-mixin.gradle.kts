32708d7fd27cea7002f24cb7f24c0b4435b457cb
Peng Cheng <peng.cheng@acyclic.ai>
2025-07-10 16:20:25 -0400

    jvmArgs applied to all test tasks

--- src/main/kotlin/ai.acyclic.scala-mixin.gradle.kts
+++ src/main/kotlin/ai.acyclic.scala-mixin.gradle.kts
@@ -111,16 +111,16 @@ allprojects {
 
             useJUnitPlatform {
                 includeEngines("scalatest")
+            }
 
-
-                /*
+            /*
 in IDE:
 --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.invoke=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/jdk.internal.ref=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/sun.nio.cs=ALL-UNNAMED --add-opens=java.base/sun.security.action=ALL-UNNAMED --add-opens=java.base/sun.util.calendar=ALL-UNNAMED
 working directory:
 $MODULE_WORKING_DIR$
-                 */
-                val addOpens =
-                    """
+             */
+            val addOpens =
+                """
                         java.base/java.lang
                         java.base/java.lang.invoke
                         java.base/java.lang.reflect
@@ -136,23 +136,22 @@ $MODULE_WORKING_DIR$
                         java.base/sun.security.action
                         java.base/sun.util.calendar
                     """.trimIndent().split("\n")
-                // from apache spark test runner
+            // from apache spark test runner
 
-                var allArgs = listOf(
-                    "-Xmx2048m",  // Maximum heap size
-                    "-XX:MaxMetaspaceSize=512m",  // Maximum metaspace size
-                    "-XX:+HeapDumpOnOutOfMemoryError",  // Dump heap on OOM
-                    "-Dfile.encoding=UTF-8",  // Set file encoding
-                )
-                for (pkg in addOpens) {
-                    allArgs += "--add-opens=$pkg=ALL-UNNAMED"
-                }
+            var allArgs = listOf(
+                "-Xmx2048m",  // Maximum heap size
+                "-XX:MaxMetaspaceSize=512m",  // Maximum metaspace size
+                "-XX:+HeapDumpOnOutOfMemoryError",  // Dump heap on OOM
+                "-Dfile.encoding=UTF-8",  // Set file encoding
+            )
+            for (pkg in addOpens) {
+                allArgs += "--add-opens=$pkg=ALL-UNNAMED"
+            }
 
 //                val str = allArgs.joinToString(" ")
 //                println("== [ScalaTest JVM arguments] == ${str}")
 
-                jvmArgs(allArgs)
-            }
+            jvmArgs(allArgs)
 
             testLogging {
 

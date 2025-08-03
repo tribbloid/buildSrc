777113aa770f2e96efe6ef3398cffee5dd59f1c3
Peng Cheng <peng.cheng@acyclic.ai>
2025-08-03 14:23:03 -0400

    remove kotlin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,6 @@ repositories {
 }
 
 dependencies {
-
-    implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:2.0.20")
     implementation("com.github.ben-manes.versions:com.github.ben-manes.versions.gradle.plugin:0.51.0")
     implementation("io.github.gradle-nexus.publish-plugin:io.github.gradle-nexus.publish-plugin.gradle.plugin:1.3.0")
     implementation("io.github.cosmicsilence.scalafix:io.github.cosmicsilence.scalafix.gradle.plugin:0.2.3")
